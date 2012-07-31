7c3960378980b3b80f72b86e5a46390fe146afe0
Nick Chase <nschase@ucsc.edu>
2012-07-30 17:51:11 -0700

    Stylizing of emailcheck.php and permutations.php

--- emailcheck.php
+++ emailcheck.php
@@ -1,4 +1,9 @@
+<div id="masterwrapper">
+<div class="title">Email Check</div>
 <form action="emailchecker.php" method="post">
-	Email Address: <input type="text" name="email" /><br/>
+	<div class="inner">Email Address:</div> <input type="text" name="email" /><br/>
 <input type="submit" />
 </form>
+</div>
+<script type="text/JavaScript" src="myjs.js"></script>
+<link rel="stylesheet" type="text/css" href="styling.css" />
--- permutations.php
+++ permutations.php
@@ -1,7 +1,12 @@
+<div id="masterwrapper">
 <form action="verify.php" method="post">
-	First Name: <input type="text" name="fname" /><br/>
-	Middle Name: <input type="text" name="mname" /><br/>
-	Last Name: <input type="text" name="lname" /><br/>
-	Domain Name: <input type="text" name="domain"><br/>
+	<div class="title">Evensale Mailing</div>
+	<div class="inner">First Name:</div> <input type="text" name="fname" /><br/>
+	<div class="inner">Middle Name:</div> <input type="text" name="mname" /><br/>
+	<div class="inner">Last Name:</div> <input type="text" name="lname" /><br/>
+	<div class="inner">Domain Name:</div><input type="text" name="domain"><br/>
 <input type="submit" />
 </form>
+</div>
+<script type="text/JavaScript" src="myjs.js"></script>
+<link rel="stylesheet" type="text/css" href="styling.css" />
